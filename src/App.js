738882a476334b1885be5f300b9a0c5dac7d467c
Pedro Silva <pedronestjp@gmail.com>
2024-07-23 23:51:52 -0300

    up/update dashboard

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
-import Header from './components/Header';
+import Questionnaire from './components/Questionnaire';
 import Dashboard from './components/Dashboard';
+import Header from './components/Header';
+import Academy from './components/Academy';
+import ActionPlans from './components/ActionPlans';
+import Reports from './components/Reports';
+import Settings from './components/Settings';
 import styles from './App.module.css';
 
 function App() {
   return (
-    <div className={styles.app}>
-      <Sidebar />
-      <div className={styles.mainContent}>
-        <Header />
-        <Dashboard />
+    <Router>
+      <div className={styles.app}>
+        <Sidebar />
+        <div className={styles.mainContent}>
+          <Header />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/academy" element={<Academy />} />
+            <Route path="/questionnaires" element={<Questionnaire />} />
+            <Route path="/action-plans" element={<ActionPlans />} />
+            <Route path="/reports" element={<Reports />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,32 +1,109 @@
 import React from 'react';
 import styles from './Dashboard.module.css';
 import Chart from './Chart';
+import mapCollaboratorsImg from '../assets/mapCollaborators.png';
+import personIcon from '../assets/person-icon.png';
+import chartDecline from '../assets/chart-decline.png';
+import chartUp from '../assets/chart-up.png';
+import { Tune, ToggleOffRounded } from '@mui/icons-material';
 
 const Dashboard = () => {
   return (
     <div className={styles.dashboard}>
-      <div className={styles.stats}>
+      <div className={styles.myChartContainer}>
+        <h3>Meu Gráfico</h3>
         <div className={styles.statCard}>
-          <h3>Engajamento dos Colaboradores</h3>
-          <p>Nível de Engajamento</p>
-          <span>40%</span>
+          <div className={styles.content}>
+            <img src={personIcon} alt="Person Icon" />
+            <div className={styles.statsTexts}>
+              <h3>Engajamento dos Colaboradores</h3>
+              <p>Mês atual, Nível de Engajamento</p>
+            </div>
+          </div>
+          <div className={styles.chartIndicator}>
+            <img src={chartDecline} alt="Chart Decline" />
+            <span>40%</span>
+          </div>
         </div>
+
         <div className={styles.statCard}>
-          <h3>Treinamentos</h3>
-          <p>% Atingido</p>
-          <span>70%</span>
+          <div className={styles.content}>
+            <img src={personIcon} alt="Person Icon" />
+            <div className={styles.statsTexts}>
+              <h3>Treinamentos</h3>
+              <p>Mês atual, % Atingido</p>
+            </div>
+          </div>
+          <div className={styles.chartIndicator}>
+            <img src={chartUp} alt="Chart Up" />
+            <span>70%</span>
+          </div>
         </div>
+
         <div className={styles.statCard}>
-          <h3>Pactuações</h3>
-          <p>meta anual</p>
-          <span>90%</span>
+          <div className={styles.content}>
+            <img src={personIcon} alt="Person Icon" />
+            <div className={styles.statsTexts}>
+              <h3>Pactuações</h3>
+              <p>Mês atual, meta anual</p>
+            </div>
+          </div>
+          <div className={styles.chartIndicator}>
+            <img src={chartUp} alt="Chart Up" />
+            <span>90%</span>
+          </div>
         </div>
       </div>
-      <div className={styles.chartSection}>
+
+      <div className={styles.statsContainer}>
+        <div className={styles.statsTitle}>
+          <h3>Estatisticas</h3>
+          <Tune />
+        </div>
+        <div className={styles.statsCards} >
+          <div className={styles.statCard}>
+            <div className={styles.content}>
+              <img src={personIcon} alt="Person Icon" />
+              <div className={styles.statsTexts}>
+                <h3>Questionário</h3>
+                <span>N%</span>
+              </div>
+            </div>
+            <div className={styles.chartIndicator}>
+              <ToggleOffRounded />
+              <span> Progresso </span>
+            </div>
+          </div>
+          <div className={styles.statCard}>
+            <div className={styles.content}>
+              <img src={personIcon} alt="Person Icon" />
+              <div className={styles.statsTexts}>
+                <h3>Checklist</h3>
+                <span>N%</span>
+              </div>
+            </div>
+            <div className={styles.chartIndicator}>
+              <ToggleOffRounded />
+              <span> Progresso </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.chartContainer}>
         <h3>Responsabilidade Socioambiental</h3>
         <Chart />
       </div>
-    </div>
+      
+      <div className={styles.mapCollaborators}>
+        <h3>Colaboradores</h3>
+        <img src={mapCollaboratorsImg} alt="mapa de local dos colaboradores" />
+        <div className={styles.reportMapCollaborators}>
+          <h3>N% Fabrica</h3>
+          <h3>N% Escritório</h3>
+        </div>
+      </div>
+    </div >
   );
 }
 
